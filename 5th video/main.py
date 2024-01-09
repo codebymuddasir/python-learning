924a7ea1f3d474b40dd07c4cdf63681926874d26
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-09 20:55:20 +0500

    comment,escaping sequence

---
+++ 5th video/main.py
@@ -0,0 +1,14 @@
+''' This is a comment '''
+print("hello world/n i am a good boy")
+
+'''THis is a triple line comment  '''
+# this is escaping sequence charachter
+print('this is print statement and "python really" good program')
+
+
+# in print we put more values
+print('hello world',2,3)
+print(22-2,22+2git )
+
+
+
