5e7ca08092e97229593c7da971a526e473994995
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-10 19:51:59 +0500

    my 6th video about variables

---
+++ 6th video -veriables-data types/main.py
@@ -0,0 +1,20 @@
+#variables and data types
+a = ("muddasir")
+print(a)
+m = 2024
+print(m)
+r = True
+print(r)
+
+t1 = 11
+print(m + t1) 
+# Types 'str'
+print("type of a is" , type(a))
+# type 'int'
+print(type(m))
+# type 'bool'
+print(type(r))
+
+# LIST
+list1 = [12, 22.32, [121, 786], ["banan", "apple"]]
+print(list1)
