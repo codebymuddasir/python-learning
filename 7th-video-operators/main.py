4199eaf3db4d44cf140324263ff6510c987bbd81
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-11 20:48:08 +0500

    my 7th video about operators

---
+++ 7th-video-operators/main.py
@@ -0,0 +1,16 @@
+# About Operators
+#Addition '+'
+print(21+21)
+#Subtraction
+print(76-46)
+#Multiplication
+print(7*5)
+#Division
+print(17/5) 
+#Exponential
+print(5**4) 
+#Modulus is percent If we divided 7 by 4 then 3 will be the remainder, so it gives the remainder
+print(7%4)
+#we use  if we only want 3 in answer of 3.4 then we use Floor division
+print(17//5) 
+
