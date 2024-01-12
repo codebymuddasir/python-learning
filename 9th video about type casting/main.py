ea6bb3e5a91df5cd68192cdc6a17f556e63b2d47
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-12 22:53:52 +0500

    My 9th video about type casting

---
+++ 9th video about type casting/main.py
@@ -0,0 +1,25 @@
+# Type casting: The conversion of one data type to another data type is called type-casting
+
+# types
+#Explicit I'm doing
+# implicit Pyton is doing
+
+# Example of explicit type casting
+a = "1"
+b = "2"
+
+print(int(a) + int(b))
+
+# Implicit TypeCasting
+c = 1.9
+d = 8
+print(c + d)
+
+
+
+
+
+
+
+
+
