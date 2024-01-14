552ab58c0c94bfda6d5f43f4d1976b20e2fabb89
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-14 15:12:59 +0500

    About input

---
+++ 10th-videio-string/main.py
@@ -0,0 +1,25 @@
+#Python allows for user input-That means we are able to ask the user for input.
+a = input()
+print("My name is", a)
+
+a = input("Enter your name: ")
+print("My name is", a)
+#Whatever is inside the 'x' will be a 'string'
+m = input("Enter first number: ")
+n = input("Enter second number: ")
+print(m + n)
+# so just type cast into 'int'
+print(int(m) + int(n))
+# All operators
+# Subtraction
+print(int(m) - int(n))
+# Multiply
+print(int(m) * int(n))
+#Division
+print(int(m) / int(n))
+#Modulus
+print(int(m) % int(n))
+#Exponentiation
+print(int(m) ** int(n))
+#Floor division
+print(int(m) // int(n))
