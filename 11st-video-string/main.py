3eb76202fcbabb6c8e0bf99b041728626f9b6de0
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-15 18:43:12 +0500

    I done my task

---
+++ 11st-video-string/main.py
@@ -0,0 +1,26 @@
+#In python,everything we close in one quatation or double quatation is consider as String
+name = "Muddasir"
+brother = "ali"
+
+print('Hello', name)
+#in python string is like an array of character
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+
+
+feature = """hi,my nam eis muddasir,
+I am a develpor,
+I done my task today
+python is really intresting."""
+print(feature)
+for character in feature:
+    print(character)
+
+
+
