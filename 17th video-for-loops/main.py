173f68af96800b959fe8414f4814f7ee8518c141
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-22 20:42:34 +0500

    FOR LOOPS

---
+++ 17th video-for-loops/main.py
@@ -0,0 +1,19 @@
+animal = "Monkey"
+for py in animal:
+    print(py)
+city = "lahore"
+for a in city:
+    print(a, end= ", ")    
+    #Range  
+for m in range(12):  
+    print(m)
+for k in range(1,20001):
+    print(k)  
+for n in range(1, 15, 3):
+    print(n)      
+    #list
+fruits = ["apple", "kiwi", "banana", "japani phal"]
+for fruit in fruits:
+    print(fruit)  
+for frui in fruit:
+  print(frui)
