7e2cdba35129f62c14a7ff054a9b4bfa9142247f
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-23 19:35:50 +0500

    While loop

---
+++ 18th video while Loop/main.py
@@ -0,0 +1,20 @@
+# While loop : while loop statemet ko us time tk execute krta hai jab tak value true hon aur jab statement false ho jai to wo execute krna band kr deta hai
+a = int(input("enter the number: "))
+while(a<=29):
+      a = int(input("enter the no. : "))
+print(a)
+
+print("end")   
+# decrementing loop
+count = 8
+while(count > 0):
+      print(count)
+      count = count- 1
+    #   agr hum count ko -1 se + 1 kar diya to ye INFINITE LOOP ban jaye ga 
+count = 8
+while(count > 0):
+      print(count)
+      count = count + 1
+
+
+      
