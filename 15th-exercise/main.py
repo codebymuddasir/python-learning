2a2f97829bb33634193837483c158e8e16fb0828
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-30 20:31:18 +0500

    list done

--- 15th-exercise/main.py
+++ 15th-exercise/main.py
@@ -1,10 +1,14 @@
-import time
-timenow = time.strftime("%H:%M:%S")
-print(timenow)
+# a = int(input("enter the number: "))
+# while(a<=20):
+#     a = int(input("enter the number: "))
+#     print("the number is greter then 20")
+#     print(a)
+# print("end")
+number = 0
 
-timenow = time.strftime("%H")
-print(timenow)
-timenow = time.strftime("%M")
-print(timenow)
-timenow = time.strftime("%S")
-print(timenow)
+while number < 20:
+    print(number)
+    number += 1
+
+print("Stopped because the number is now greater than or equal to 20.")
+ 
