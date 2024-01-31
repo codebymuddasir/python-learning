58c25ccb5d93a43aa1f23a6a2642d236ebad7811
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-01-31 18:30:11 +0500

    list method

--- 22th -video-list/main.py
+++ 22th -video-list/main.py
@@ -1,23 +1,4 @@
 classe = [1,2,3]
-print(classe)
-print(type(classe))
-print(classe[0])
-print(classe[1])
-print(classe[2])
-# list index
-
-
-colours = ["blue", "red", "yellow", "white"]
-print(colours[0])
-print(colours[1])
-print(colours[2])
-print(colours[3])
-# NEGATIVE INDEX
-print(classe[-2])
-lst = [m for i in range(10)]
-print(lst)
-lst = [m for i in range(10) if i%2==0]
-print(lst)
 
 
 
---
+++ 23rd-video-list method/main.py
@@ -0,0 +1,24 @@
+# list method
+list = [32,23,23,21,33,12]
+print(list)
+# append
+list.append(7)
+print(list)
+# reverse
+list.reverse()
+print(list)
+# sort
+list.sort(reverse=True)
+print(list)
+# extend
+list.extend([2121, 2019, 2220])
+print(list)
+# insert
+list.insert(2098)
+print(list)
+# concatenating two list
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+ck = list1 + list2
+print(ck)
+
