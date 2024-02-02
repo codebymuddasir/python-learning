74fc8713e886edc18e247c7f742054df8d0d7dd3
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-02-02 17:52:20 +0500

    idone my task

---
+++ 23rd-video-list method/main.py
@@ -0,0 +1,4 @@
+# TUPLES: tuples are ordered collection of data time.tuples is separated by commas "," and under the round bracket().tuples are unchangeable
+tupl = (9,32,12)
+print(tupl)
+print(type(tupl))
