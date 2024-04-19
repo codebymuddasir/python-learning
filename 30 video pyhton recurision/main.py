56042315744578da55e17d31ea65d7eaa6a76906
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-04-19 19:13:26 +0500

    i done my task

--- 30 video pyhton recurision/main.py
+++ 30 video pyhton recurision/main.py
@@ -1,1 +1,22 @@
-# the docstring is help u to understand function easily
+# factorial(7) = 7*6*5*4*3*2*1
+# factorial(6) = 6*5*4*3*2*1
+
+
+
+# factorial(n) = n * factorial(n-1)
+def factorial(n):
+  if (n == 0 or n == 1):
+    return 1
+  else:
+    return n * factorial(n - 1)
+print(factorial(3))
+print(factorial(2))
+print(factorial(6))
+
+print(factorial(5))
+5 * factorial(4)
+5 * 4 * factorial(3)
+5 * 4 * 3 * factorial(2)
+5 * 4 * 3 * 2 * factorial(1)
+5 * 4 * 3 * 2 * 1
+
