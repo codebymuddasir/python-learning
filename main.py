f8b627c2c4421b9124733a3e13dd82b5186d3a60
Muhammad Muddasir <155835634+codebymuddasir@users.noreply.github.com>
2024-04-20 19:11:19 +0500

    Add files via upload
    i donr my task

---
+++ main.py
@@ -0,0 +1,9 @@
+# SET: Set are spearated with comma and enclosed in curly bracket. once set created it cant be changed. set cant contain duplicate value.
+# Set cant be in order 
+# like 
+s = {2,3,4,5,6,4}
+print(s)
+math = {5,"muddasir",2.3,5,"false"}
+print(math)
+for value in math:
+    print(value)
