fd4ff0fe60145db10c9844af4a4cc2264cfa72ed
Muhammad Muddasir <muddasirdaha@gmail.com>
2024-04-22 18:27:31 +0500

    Dictionary

---
+++ python-learning#33-methodof-dictionary/main.py
@@ -0,0 +1,27 @@
+# dictionary
+#  we can use this for result, we can check roll number wise grade
+dict = {"Table": "Object", 
+     "Ronaldo": "Human"   
+}
+print(dict["Ronaldo"])
+
+# employ id 
+dict = {
+    1:"Muddasir",
+    22:"Ahmad",
+    12:"Kashan"
+}
+print(dict[1])
+print(dict[22])
+print(dict[12])
+# accesing single value
+data = {"name":"ashish","age":14, "height":"5'7" }
+print(data)
+print(data["height"])
+print(data["name"])
+data = {"name":"ashish","age":14, "height":"5'7" }
+print(data)
+print(data.keys())
+print(data.values())
+print(data.items())
+      
