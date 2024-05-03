1e9b7bc868a4385070269dfdeb899a4ebe6f64d6
Muhammad Muddasir <155835634+codebymuddasir@users.noreply.github.com>
2024-05-03 20:11:08 +0500

    Add files via upload
    I learn about Enumerate

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
-sc = (input("Enter the message:"))
-words = sc.split(" ")
-
-coding = input("1 for coding or 0 for decoding")
-coding = True if  (coding=="1") else False
-print(coding)
-if(coding):
-    nwords = []
-    for word in words:
-        if(len(word)>=3):
-            m1 = "asd"
-            m2 = "fgh"
-            scnew = m1= word[1:] + word[0] + m2
-            nwords.append(scnew)
-        else:
-            nwords.append(word[::-1])
-            print(" ".join(nwords))
+marks = [1, 2, 3, 4, 66, 79, 12]
 
 
+for index, mark in enumerate(marks):
 
-            
+    print(mark)
 
-            
+    if(index == 4):
+     print("Muddasir, Awesom")
+
+fruits = ("apple", "banana", "kiwi" )  
+for index, fruit in enumerate(fruits)  :
+   print(fruit)
+   if(index == 2):
+      print("I love it")
+      
